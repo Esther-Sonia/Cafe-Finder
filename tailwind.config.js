ae89420b976b4b464b52517d566071650be2b1a5
Esther-Sonia <esthersonia21@gmail.com>
2025-08-29 09:51:21 +0300

    add tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // 👈 scans all your React components
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
